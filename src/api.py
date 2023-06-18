67c990c7962084a0fb71307834aa050f313235d1
Uliana Semyonova <i@ulianasemyonova.ru>
2023-06-18 19:24:58 +0500

    Update api.py > Add docstrings to get_vacancies methods > create get_vacancies methods for SuperJobAPI and HeadHunterAPI classes

--- src/api.py
+++ src/api.py
@@ -1,30 +1,69 @@
+import os
 from abc import ABC, abstractmethod
+import requests
 
 
 class API(ABC):
     """
     Абстрактный класс для работы с API сайтов
     """
-    @abstractmethod
-    def connect_to_api(self):
-        pass
 
     @abstractmethod
-    def get_jobs(self):
+    def get_vacancies(self, profession):
+        """
+        Абстрактный метод для получения словаря с данными о вакансиях
+        :param profession: название должности
+        """
         pass
 
 
 class SuperJobAPI(API):
-    def connect_to_api(self):
-        pass
+    """
+    Класс для работы с API сайта SuperJob
+    """
 
-    def get_jobs(self):
-        pass
+    URL = 'https://api.superjob.ru/2.0/vacancies/'
+    HEADERS = {
+        "X-Api-App-Id": os.getenv('SUPERJOB_API_KEY')
+    }
+
+    def get_vacancies(self, profession: str):
+        """
+        Получает и возвращает словарь с данными о вакансиях
+        :param profession: название должности
+        :return: словарь с данными о вакансиях
+        """
+        profession = profession.strip()
+
+        params = {
+            "keyword": profession
+        }
+
+        vacancies = requests.get(self.URL, headers=self.HEADERS, params=params)
+
+        return vacancies.json()
 
 
 class HeadHunterAPI(API):
-    def connect_to_api(self):
-        pass
+    """
+    Класс для работы с API сайта HeadHunter
+    """
 
-    def get_jobs(self):
-        pass
+    URL = 'https://api.hh.ru/vacancies'
+
+    def get_vacancies(self, profession: str):
+        """
+        Получает и возвращает словарь с данными о вакансиях
+        :param profession: название должности
+        :return: словарь с данными о вакансиях
+        """
+        profession = profession.strip()
+
+        params = {
+            "text": profession,
+            "search_field": 'name'
+        }
+
+        vacancies = requests.get(self.URL, params=params)
+
+        return vacancies.json()
