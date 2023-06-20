d028e2a1b852f7d5acde29957c9fca6e368c27ba
Uliana Semyonova <i@ulianasemyonova.ru>
2023-06-20 19:53:39 +0500

    s

--- src/vacancy.py
+++ src/vacancy.py
@@ -77,3 +77,39 @@ class Vacancy:
     @property
     def work_address(self):
         return self.__work_address
+
+    @classmethod
+    def add_to_class(cls, vacancies_data: dict):
+        """
+        Инициализирует экземпляры класса Vacancy из переданных данных
+        :param vacancies_data: данные о вакансиях
+        """
+        if vacancies_data.get('objects') is not None:
+            if vacancies_data['objects'] != [] and vacancies_data['total'] != 0:
+
+                for vacancy in vacancies_data['objects']:
+                    cls(vacancy['profession'], int(vacancy['payment_from']), vacancy['link'], vacancy['candidat'],
+                        vacancy['address'])
+
+        if vacancies_data.get('items') is not None:
+            if vacancies_data['items'] != [] and vacancies_data['found'] != 0:
+
+                for vacancy in vacancies_data['items']:
+                    if vacancy['salary'] is None and vacancy['address'] is None:
+                        salary = 0
+                        address = 'Нет информации об адресе'
+                    elif vacancy['salary'] is None:
+                        salary = 0
+                        address = vacancy['address']['raw']
+                    elif vacancy['address'] is None:
+                        salary = int(vacancy['salary']['from'])
+                        address = 'Нет информации об адресе'
+                    else:
+                        try:
+                            salary = int(vacancy['salary']['from'])
+                        except TypeError:
+                            salary = 0
+                        address = vacancy['address']['raw']
+
+                    cls(vacancy['name'], salary, vacancy['url'], vacancy['snippet']['requirement'] + '\n' +
+                        vacancy['snippet']['responsibility'], address)
