564e50f4dfed1826b51f79a2cae687e3d2f86af7
Uliana Semyonova <i@ulianasemyonova.ru>
2023-07-02 22:14:29 +0500

    Update vacancy.py > create CurrencyTranslator class add currency check in validate_salary methods of HeadHunterVacancyDataValidator and SuperJobVacancyDataValidator classes

--- src/vacancy.py
+++ src/vacancy.py
@@ -1,5 +1,8 @@
+import os
 import re
 from abc import ABC, abstractmethod
+
+import requests
 
 
 class Vacancy:
@@ -93,36 +96,36 @@ class Vacancy:
         return self.__work_address
 
     @classmethod
-    def add_to_class_from_superjob(cls, vacancies_data: dict):
+    def add_to_class_from_superjob(cls, vacancies_data):
         """
         Инициализирует экземпляры класса Vacancy из переданных данных API SuperJob
         :param vacancies_data: данные о вакансиях
         """
         validator = SuperJobVacancyDataValidator()
 
-        if vacancies_data['objects'] != [] and vacancies_data['total'] != 0:
+        if vacancies_data:
 
-            for vacancy in vacancies_data['objects']:
+            for vacancy in vacancies_data:
                 address = validator.validate_address(vacancy['address'])
                 profession = validator.validate_profession(vacancy['profession'])
-                salary_from = validator.validate_salary(vacancy['payment_from'])
-                salary_to = validator.validate_salary(vacancy['payment_to'])
+                salary_from = validator.validate_salary(vacancy['payment_from'], vacancy['currency'])
+                salary_to = validator.validate_salary(vacancy['payment_to'], vacancy['currency'])
                 url = validator.validate_url(vacancy['link'])
                 requirements = validator.validate_vacancy_requirement(vacancy['candidat'])
 
                 cls(profession, salary_from, salary_to, url, requirements, address)
 
     @classmethod
-    def add_to_class_from_headhunter(cls, vacancies_data: dict):
+    def add_to_class_from_headhunter(cls, vacancies_data):
         """
         Инициализирует экземпляры класса Vacancy из переданных данных API HeadHunter
         :param vacancies_data: данные о вакансиях
         """
         validator = HeadHunterVacancyDataValidator()
 
-        if vacancies_data['items'] != [] and vacancies_data['found'] != 0:
+        if vacancies_data:
 
-            for vacancy in vacancies_data['items']:
+            for vacancy in vacancies_data:
                 profession = validator.validate_profession(vacancy['name'])
                 address = validator.validate_address(vacancy['address'])
                 salary_from, salary_to = validator.validate_salary(vacancy['salary'])
@@ -211,7 +214,7 @@ class VacancyDataValidator(ABC):
 
     @staticmethod
     @abstractmethod
-    def validate_salary(salary):
+    def validate_salary(salary, currency):
         """
         Абстрактный метод для проверки правильности формата зарплаты
         """
@@ -260,16 +263,23 @@ class SuperJobVacancyDataValidator(VacancyDataValidator):
         return profession
 
     @staticmethod
-    def validate_salary(salary):
+    def validate_salary(salary, currency):
         """
         Проверяет правильность формата зарплаты и возвращает в нужном формате
+        :param currency: валюта
         :param salary: данные о зарплате
         :return: данные о зарплате в int
         """
+        rate = 1
+
+        if currency != 'rub':
+            translator = CurrencyTranslator()
+            rate = translator.get_exchange_rate(currency.upper())
+
         if salary is None:
             salary = 0
 
-        return int(salary)
+        return round(int(salary) / rate)
 
     @staticmethod
     def validate_address(address):
@@ -326,15 +336,17 @@ class HeadHunterVacancyDataValidator(VacancyDataValidator):
         return profession
 
     @staticmethod
-    def validate_salary(salary):
+    def validate_salary(salary, currency=None):
         """
         Проверяет правильность формата зарплаты и возвращает в нужном формате
+        :param currency: валюта
         :param salary: данные о зарплате
         :return: данные о зарплате в int
         """
         if salary is None:
             return 0, 0
 
+        rate = 1
         salary_from = salary['from']
         salary_to = salary['to']
 
@@ -344,7 +356,14 @@ class HeadHunterVacancyDataValidator(VacancyDataValidator):
         if salary_to is None:
             salary_to = 0
 
-        return int(salary_from), int(salary_to)
+        if salary.get('currency') is not None:
+            currency = salary['currency']
+
+            if currency != 'RUR':
+                translator = CurrencyTranslator()
+                rate = translator.get_exchange_rate(currency)
+
+        return round(int(salary_from) / rate), round(int(salary_to) / rate)
 
     @staticmethod
     def validate_address(address):
@@ -404,3 +423,33 @@ class HeadHunterVacancyDataValidator(VacancyDataValidator):
             responsibility = ''
 
         return f'{requirement} {responsibility}'
+
+
+class CurrencyTranslator:
+    """
+    Класс для получения курса валют
+    """
+    URL = "https://api.apilayer.com/exchangerates_data/latest"
+    PARAMS = {
+        'base': 'RUB'
+    }
+    HEADERS = {
+        "apikey": os.environ.get('EXCHANGE_RATE_API_KEY')
+    }
+
+    def get_exchange_rate(self, currency):
+        """
+        Возвращает курс валюты
+        :return: курс валюты
+        """
+        currency = currency.upper()
+        response = requests.request("GET", self.URL, headers=self.HEADERS, params=self.PARAMS)
+
+        status_code = response.status_code
+
+        if status_code == 200:
+            result = response.json()
+        else:
+            return 1
+
+        return result['rates'][currency]
